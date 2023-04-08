fb4b3aa0b77da5879e58113966967a75225d9f06
Israel Gonzalez <igonzals@gmail.com>
2023-04-07 21:20:04 -0600

    Ignore, package, NextJS, Tailwind and the index page for this project

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # travel-community
 Advanced Web Technologies Group Project
+
+## Instalation
+
+´´´
+npm init
+npm install next react react-dom
+npm install eslint eslint-config-next --save-dev
+´´´
+Make:
+
+npm run dev
+
+Project should listen in http://localhost:3000
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '../components/Card';
+
+export default function Home() {
+  return (
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <Card>
+        <h1 className="text-2xl font-bold">Hello, Project Travel Community!</h1>
+      </Card>
+    </div>
+  );
+}
