126216c9bd9eb4ec9cd0aad39f25ae6e8c234bc0
mengyang1996 <111538504+mengyang1996@users.noreply.github.com>
2023-05-05 09:50:22 -0600

    can show UI component now

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
-import connectDB from '../utils/db';
+//import connectDB from '../utils/db';
 import Layout from "../components/layouts/Layout";
 import TopNavBar from "../components/common/TopNavBar";
 import BannerPlaces from "../components/common/BannerPlaces";
+import LogIn from "../components/pages/login/LogIn"
 
 const loginPage = (props) => {
   //JS functions
   
   return(
     <>
-      <TopNavBar></TopNavBar>
-      <BannerPlaces></BannerPlaces>
-      <h1>Login form</h1>
+     <TopNavBar></TopNavBar>
+     <BannerPlaces></BannerPlaces>
+     <LogIn></LogIn>
+
+
     </>
   )
 }
@@ -19,7 +22,15 @@ const loginPage = (props) => {
 export default loginPage;
 
 loginPage.getLayout = function getLayout(page, props) {
-  return (
-    <Layout headerData={props.headerData}>{page}</Layout>
-  );
-};
+    return (
+      <Layout headerData={props.headerData}>{page}</Layout>
+    );
+  };
+  
+  export async function getStaticProps() {
+    return {
+      props: {
+        headerData:{title: "Travel Community -  Create a Trip", content: "You can create a Trip"},
+      }
+    };
+  }
--- pages/signup.js
+++ pages/signup.js
@@ -1,3 +1,30 @@
-import connectDB from '../utils/db';
 import Layout from "../components/layouts/Layout";
 import TopNavBar from "../components/common/TopNavBar";
+import BannerPlaces from "../components/common/BannerPlaces";
+import SignUpForm from "../components/pages/sign/SignUpForm"
+
+const signUp = (props) => {
+  return (
+    <>
+      <TopNavBar></TopNavBar>
+      <BannerPlaces></BannerPlaces>
+      <SignUpForm></SignUpForm>
+    </>
+  )
+}
+
+export default signUp;
+
+signUp.getLayout = function getLayout(page, props) {
+  return (
+    <Layout headerData={props.headerData}>{page}</Layout>
+  );
+};
+
+export async function getStaticProps() {
+  return {
+    props: {
+      headerData:{title: "Travel Community -  Sign Up", content: "Sign Up"},
+    }
+  };
+}
